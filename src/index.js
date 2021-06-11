ce7ea8b7bc901c452ad6df50849cd479f2a70341
Jady Hom <hyjady@gmail.com>
2021-06-11 19:58:02 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import RoosterGrin from "./RoosterGrin";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RoosterGrin />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
